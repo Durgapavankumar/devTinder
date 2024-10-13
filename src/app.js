4952d6596201ad11debba3290f5a5336c920086d
Durgapavankumar <pailladurgapavankumar@gmail.com>
2024-10-13 14:08:13 -0500

    exploring post get and etc

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from server");
+// app.use("/", (req, res) => {
+//   res.send("Hello from server");
+// });
+
+app.get("/user", (req, res) => {
+  //logic for get
+  res.send("Get from the server");
+});
+app.post("/user", (req, res) => {
+  res.send("Post from the server (updated");
 });
 
-app.use("/home", (req, res) => {
-  res.send("That' how different requests are handled properly");
-});
+// app.use("/home", (req, res) => {
+//   res.send("That' how different requests are handled properly");
+// });
+// app.use("/test", (req, res) => {
+//   res.send("Hello from testtttt");
+// });
 
 app.listen(3000, () => {
   console.log("Server is running successfully");
