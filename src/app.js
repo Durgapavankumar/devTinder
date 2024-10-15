391d3f903b64c9f097b4ba26134fae7d7ff61733
Durgapavankumar <pailladurgapavankumar@gmail.com>
2024-10-14 20:48:33 -0500

    Created API that finds the users using email and using find method to fetch all the user

--- src/app.js
+++ src/app.js
@@ -19,6 +19,30 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/email", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    console.log(userEmail);
+    const user = await User.find({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("Error not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.send("Error finding the user");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(404).send("Error not found");
+  }
+});
+
 connectDb()
   .then(() => {
     console.log("Successfully connected to the database");
